dd0479710635bca969e5a350fdb02cb4c102345c
KananAmir <amirovknn@gmail.com>
2024-10-29 09:24:29 +0400

    js operators

--- 10.28.js-intro. variables. data types. operators/operators.js
+++ 10.28.js-intro. variables. data types. operators/operators.js
@@ -9,8 +9,7 @@ let division = num1 / num2;
 let square = num1 ** 2;
 let reminder = num2 % num1;
 
-console.log(reminder);
-
+// console.log(reminder);
 let count = 5;
 
 // count = count + 1;
@@ -18,7 +17,7 @@ let count = 5;
 // count = count - 1;
 count--;
 
-console.log(count);
+// console.log(count);
 
 let number = 16;
 
@@ -26,8 +25,37 @@ number += 10;
 number = number * 10;
 number * 10;
 
-console.log(count == "4");
+// console.log(count == "4");
 
 let bool = true;
 
-console.log(!bool);
+// console.log(!bool);
+
+let a = 0;
+// console.log(a++);
+// console.log(++a);
+
+// console.log(--a);
+// console.log(a);
+
+// console.log(5 > 3);
+// console.log(5 > 33);
+
+// console.log("a" > "b"); //false
+// console.log("zubeyda" > "feyza"); //true
+// console.log("aaa" > "aaa"); //false
+// console.log("aaa" === "aaa"); //true
+
+//Logical Operators
+
+console.log(true && "hello" && null); //null
+console.log("bp109" && undefined && "hello"); //undefined
+console.log(NaN && "hello" && false); // NaN
+console.log("false" && true && "hello"); // "hello"
+console.log(false && null && true); // false
+
+console.log(true || "hello" || null); //true
+console.log("bp109" || undefined || "hello"); //bp109
+console.log(NaN || "hello" || false); // "hello"
+console.log("false" || true || "hello"); // "false"
+console.log(false || null || undefined); // undefined
